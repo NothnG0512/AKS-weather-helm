94988266dd5e45d8595d7dbdc30e2586122c8b25
Anuj Pal <Anuj.Pal@Liquid.tech>
2026-02-18 00:28:33 +0530

    feat: add github actions pipeline for docker build

--- Azure-AKS/Terraform/main.tf
+++ Azure-AKS/Terraform/main.tf
@@ -25,3 +25,14 @@ resource "azurerm_kubernetes_cluster" "aks" {
     Environment = "DevOps-Mission"
   }
 }
+resource "azurerm_container_registry" "acr" {
+  name = "weatherappregistry2026"
+  resource_group_name = azurerm_resource_group.rg.name
+  location = azurerm_resource_group.rg.location 
+  sku = "Standard"
+  admin_enabled = true
+
+}
+output "acr_login_server"{
+  value = azurerm_container_registry.acr.login_server
+} 
